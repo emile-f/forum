52807ec501eba405da16e59056283a8eba61ab50
emile ferrand <emile@storied.co>
2020-10-19 16:06:11 -0700

    Add loader and hide test files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Loader from "react-loader-spinner";
 
 function App() {
   return (
     <div className="App">
+      <Loader
+        type="Puff"
+        color="#00BFFF"
+        height={100}
+        width={100}
+        timeout={3000} //3 secs
+        className="loader"
+      />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
