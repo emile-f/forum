04f06715ea11241176b9fa56df3f59979a3b3357
emile ferrand <emile@storied.co>
2020-11-01 13:27:44 -0800

    Deploy frontend

---
+++ precache-manifest.0869fac018491ba084ce71850cf0567b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2aa316a38b9e072077a33988ae8ea44c",
+    "url": "/forum/index.html"
+  },
+  {
+    "revision": "4d2fe338e8611a0fad89",
+    "url": "/forum/static/css/main.c52e6acd.chunk.css"
+  },
+  {
+    "revision": "7e224c335e8df0c80128",
+    "url": "/forum/static/js/3.020e522b.chunk.js"
+  },
+  {
+    "revision": "dab48965ef1ac6d4423fc0e3f4be62d7",
+    "url": "/forum/static/js/3.020e522b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df96f607fc25d4195f78",
+    "url": "/forum/static/js/4.d72da488.chunk.js"
+  },
+  {
+    "revision": "9c431466b609b39604cc",
+    "url": "/forum/static/js/5.c2233374.chunk.js"
+  },
+  {
+    "revision": "20e9d7e46850cbd15bb6",
+    "url": "/forum/static/js/6.14ac8c2b.chunk.js"
+  },
+  {
+    "revision": "e3b93feea552d5fd932a1758b1da5f1a",
+    "url": "/forum/static/js/6.14ac8c2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d2fe338e8611a0fad89",
+    "url": "/forum/static/js/main.c383a151.chunk.js"
+  },
+  {
+    "revision": "b58fd47a1a50a57471ac",
+    "url": "/forum/static/js/runtime-main.9d919a27.js"
+  },
+  {
+    "revision": "2a94a0d75203034b54a2",
+    "url": "/forum/static/js/xlsx.0bd349ff.chunk.js"
+  }
+]);
