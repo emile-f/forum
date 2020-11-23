c5a99197a68b54f466dd5fb4d69195a5cde1d194
emile ferrand <emile@storied.co>
2020-11-22 16:31:22 -0800

    Generate new build

--- build/static/js/xlsx.2937d8ff.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpforum=this.webpackJsonpforum||[]).push([[2],{532:function(n,o){},616:function(n,o){},617:function(n,o){}}]);
-//# sourceMappingURL=xlsx.2937d8ff.chunk.js.map
---
+++ build/static/js/xlsx.49eab742.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpforum=this.webpackJsonpforum||[]).push([[2],{520:function(n,o){},613:function(n,o){},614:function(n,o){}}]);
+//# sourceMappingURL=xlsx.49eab742.chunk.js.map
